888f0684255fc2b7f032e06f0e0667712a8489db
Ethan Chen <ktun7@mail.ccsf.edu>
2021-01-10 21:45:11 -0800

    Updated the sql file

---
+++ hteiktanv2.sql
@@ -0,0 +1,17 @@
+CREATE TABLE address(
+   address_id  INT(11) NOT NULL Primary Key,
+   city VARCHAR(255) NOT NULL,
+   city VARCHAR(255) NOT NUL,
+   pincode VARCHAR(255) NULL   
+);
+CREATE TABLE employee(
+   emp_id INT(11) NOT NULL Primary Key,
+   employee_name VARCHAR(255) NOT NULL,
+   age INT(11) NOT NULL,
+   gender VARCHAR(255) NOT NULL,
+   department VARCHAR(255) NOT NULL,
+   address INT(11) null,
+   start_date DATE NOT NULL,
+   end_date DATE null,
+   CONSTRAINT Emp_FK Foreign key (address) REFERENCES address(address_id)
+);
